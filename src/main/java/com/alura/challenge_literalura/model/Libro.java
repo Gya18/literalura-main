09d75bc9e157118e33cdcec14c298dd8d6a511d0
Gian Carlo Chavez Santos <156938779+Gya18@users.noreply.github.com>
2024-07-04 23:54:02 -0500

    challenge_literalura

---
+++ src/main/java/com/alura/challenge_literalura/model/Libro.java
@@ -0,0 +1,57 @@
+package com.alura.challenge_literalura.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "libros")
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(unique = true)
+    private String titulo;
+    private String idioma;
+    private int numeroDescargas;
+
+    @ManyToOne
+    private Autor autor;
+
+        public Libro(DatosLibro datosLibro) {
+        this.titulo = datosLibro.titulo();
+        this.idioma = datosLibro.idiomas().get(0);
+        this.numeroDescargas = datosLibro.numeroDescargas();
+        this.autor = datosLibro.autores().stream().map(Autor::new).toList().get(0);
+
+    }
+
+    public void addAutor(Autor autor) {
+        autor.getLibros().add(this);
+    }
+
+    @Override
+    public String toString() {
+        return """
+               -----LIBRO-----
+               Titulo: """ + titulo + "\n" +
+                "Autor: " + autor.getNombre() + "\n" +
+                "Idioma: " + idioma + "\n" +
+                "Numero de descargas: " + numeroDescargas + "\n" +
+                "--------------";
+
+    }
+}
